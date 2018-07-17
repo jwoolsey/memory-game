038e8db76add65e5a3e3db296f36aa385f890cd2
Jennifer Woolsey <jennifer.a.woolsey@gmail.com>
2018-07-16 21:00:43 -0500

    Clean code and comments

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+window.onload = function() {
+  init();
+}
+
+//initialize game
+function init() {
+  //Call shuffle to reorder the array of cards
+  shuffle(cards);
+  //call the gameboard function to show the cards
+  gameboard();
+  //initialize timer
+  startClock();
+}
+
+/*
+* Global Variables
+*/
+
 //create array of card values
 var cards = [
               'fa-diamond', 'fa-diamond',
@@ -10,15 +28,22 @@ var cards = [
               'fa-bomb', 'fa-bomb',
             ];
 
-//Call shuffle to reorder the array
-shuffle(cards);
+var deck = document.querySelector('.deck');
+var openCards = []; //initialize open card array
+var moveCount = 0; //Initial move count
+var starCount = 3; //initial star rating
+var totalSeconds = 0; //sets initial timer to 00:00
+var gameTimer;
+var matched = 0; //match cards
+
+/*
+* Functions for Game Play
+*/
 
 //create card from array values
 function cardList(card) {
   return `<li class="card"><i class="fa ${card}"></i></li>`;
 }
-
-var deck = document.querySelector('.deck');
 
 //function to create gameboard
 function gameboard() {
@@ -28,11 +53,6 @@ function gameboard() {
   deck.innerHTML = cardHTML.join('');
   return deck;
 }
-//call the gameboard function to show the cards
-gameboard();
-
-//initialize open card array
-var openCards = [];
 
 //Add event listener for card clicks to initiate showing symbol and add to open card list
 deck.addEventListener('click', evt => {
@@ -59,9 +79,7 @@ function flip(clicked) {
   clicked.classList.toggle('show');
 }
 
-//Set initial moves to zero and adjust move count
-let moveCount = 0;
-
+//Increment moves and update display
 function moves() {
   moveCount++;
   const moveText = document.querySelector('.moves');
@@ -69,24 +87,21 @@ function moves() {
   return moveCount;
 }
 
-let starCount = 3;
-
+//Update star display based on number of moves
 function starRating(moveCount) {
-  if (moveCount === 24) {
-    //hide the third star after 24 moves
+  if (moveCount === 21) {
+    //hide the third star after 21 moves
     $('#thirdStar').hide();
     starCount = 1;
-    //hide second star after 12 moves
-  } else if (moveCount === 12) {
+    //hide second star after 14 moves
+  } else if (moveCount === 14) {
     $('#secondStar').hide();
     starCount = 2;
   }
 }
 
-let totalSeconds = 0; //sets initial timer to 00:00
-let gameTimer;
-
-$('.deck').one('click', function startTimer() {
+//timer function to give minutes and seconds
+function startTimer() {
   gameTimer = setInterval(() => {
     totalSeconds++;
     //calculate minutes from seconds
@@ -100,16 +115,21 @@ $('.deck').one('click', function startTimer() {
     }
     document.getElementById('timer').innerHTML = minute + ":" + seconds;
   }, 1000);//interval = 1 second, timer appears after one second at count 00:01
-});
-// TODO: add clock stop function on endGame & reset timer on refresh
+}
 
+//function to initialize timer on first click of deck
+function startClock() {
+  $('.deck').one('click', function() {
+    startTimer()
+  });
+}
+
+//function to stop the timer
 function stopClock() {
   clearInterval(gameTimer);
 }
 
-//match cards
-var matched = 0;
-
+//check for matching cards
 function matchCards(clicked) {
   if (
     openCards[0].firstElementChild.className ===
@@ -119,11 +139,11 @@ function matchCards(clicked) {
   } else {
     setTimeout(function() {
       closeCard(openCards);
-    }, 1000);//delays calling closeCard so you can see the flipped card
+    }, 500);//delays calling closeCard so you can see the flipped card
   }
   setTimeout(function() {
     openCards = [];
-  }, 1001);//resets 1 milliseconds after the closeCard function is called
+  }, 501);//resets 1 milliseconds after the closeCard function is called
 }
 
 //Adds 'match' class to open cards
@@ -132,7 +152,9 @@ function setMatch(openCards) {
   openCards[0].classList.toggle('match');
   matched += 2;
   if (matched === 16) {
-    endGame();
+    setTimeout(function() {
+      endGame();
+    }, 500);
   }
 }
 
@@ -149,14 +171,29 @@ $(".fa-repeat").click(function() {
   refresh();
 });
 
-//resets deck to 'card' class only
+//resets game
 function refresh() {
+  //reset timer
   totalSeconds = 0;
+  document.getElementById('timer').innerHTML = '00:00';
+
+  //reset move counter
   moveCount = 0;
+  document.querySelector('.moves').innerHTML = moveCount;
+
+  //reset stars
+  starCount = 3;
+  $('#secondStar').show();
+  $('#thirdStar').show();
+
+  //reset card classList
   const matchedCards = document.querySelectorAll('.deck li');
   for (let match of matchedCards) {
     match.className = 'card';
   }
+  matched = 0;
+
+  init();
 }
 
 // Get the modal
@@ -179,8 +216,8 @@ window.onclick = function(event) {
 
 //Add event listener for modal button to call refresh and close modal
 document.querySelector('.modalReplay').addEventListener('click', () => {
-  refresh();
   modal.style.display = 'none';
+  refresh();
 });
 
 //Add stats to the Modal
@@ -197,11 +234,10 @@ function writeStats() {
 
 //game over
 function endGame() {
-  //display modal
   stopClock();
   writeStats();
+  //display modal
   modal.style.display = 'block';
-
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -215,6 +251,5 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
